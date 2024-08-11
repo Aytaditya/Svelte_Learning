6ae5c8b66fb518d64b4fac8d48fda7df5a403a31
Aytaditya <adityaaryan531@gmail.com>
2024-08-12 00:29:19 +0530

    buttons in svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,20 @@
 <script>
 	import Nested from './Nested.svelte';
     import './app.css';
+    import Buttons from './Buttons.svelte';
+
+    let string = `this string contains some <strong>HTML!!!</strong>`;
+    // this string will be used as strong html content if @html is used if not it will show as plain text ending with </strong>
 </script>
 
 <p>This is a paragraph.</p>
 <Nested />
+<p>{@html string}</p>
+<p class="bg">{string}</p>
+
+<Buttons/>
+
+
 
 <style>
 	p {
@@ -12,4 +22,8 @@
 		font-family: 'Comic Sans MS', cursive;
 		font-size: 2em;
 	}
+    .bg{
+        background-color: rgb(237, 169, 169);
+    }
+
 </style>
---
+++ src/routes/Buttons.svelte
@@ -0,0 +1,19 @@
+<script>
+    let count=0;
+    function increase(){
+        count=count+1;
+    }
+</script>
+
+
+<button on:click={increase}>hiiii {count} {count===1 ? "time" :"times"}</button>
+
+<style>
+    button{
+        border-radius: 10px;
+        color: goldenrod;
+        margin-top: 20px;
+        font-family: 'Comic Sans MS', cursive;
+        font-size: 2em;
+    }
+</style>
