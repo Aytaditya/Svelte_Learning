32fc3af6e6a449cc4088f11b3b97d9092c46fdb7
Aytaditya <adityaaryan531@gmail.com>
2024-08-15 17:02:29 +0530

    props

--- src/routes/Nested.svelte
+++ src/routes/Nested.svelte
@@ -1,1 +1,7 @@
-<h1>hii to adii</h1>
+<script>
+    import Propss from './Propss.svelte';
+</script>
+
+<h1>Props file here</h1>
+<Propss name="Aditya"/>
+
---
+++ src/routes/Propss.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let name;
+</script>
+
+<div class="flex">
+<p>my name is {name}</p>
+<h2>Props appended above</h2>
+</div>
+
+
+<style>
+    p,h2{
+        padding: 20px;
+        font-size: 2em;
+        background-color: antiquewhite;
+    }
+    .flex{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
